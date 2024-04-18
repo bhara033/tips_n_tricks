0b98fb9e672a85950e7b62c5654d1f93dff265a7
bhara033 <135868955+bhara033@users.noreply.github.com>
2024-04-18 10:40:16 -0500

    Add files via upload
    Fast setup of R packages after completing a R update

---
+++ tt1.R
@@ -0,0 +1,14 @@
+# before upgrading to new version of R ----
+
+tmp <- installed.packages()
+installedpkgs <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+save(installedpkgs, file = "installed_old.rda")
+
+# after upgrading to new version of R ----
+
+load("installed_old.rda")
+tmp <- installed.packages()
+installedpkgs.new <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
+missing <- setdiff(installedpkgs, installedpkgs.new)
+install.packages(missing)
+update.packages()
