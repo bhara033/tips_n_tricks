0665bedff96925bd867bb5e1779a02576b0509f0
bhara033 <135868955+bhara033@users.noreply.github.com>
2024-04-29 11:15:15 -0500

    Update tt1.R
    added method 2

--- tt1.R
+++ tt1.R
@@ -1,3 +1,5 @@
+# Method 1 ----
+
 # before upgrading to new version of R ----
 
 tmp <- installed.packages()
@@ -12,3 +14,15 @@ installedpkgs.new <- as.vector(tmp[is.na(tmp[,"Priority"]), 1])
 missing <- setdiff(installedpkgs, installedpkgs.new)
 install.packages(missing)
 update.packages()
+
+# Method 2 ----
+
+# before upgrading to new version of R ----
+
+mypks <- pacman::p_lib()
+saveRDS(mypks, "~/mypks.rds")
+
+# after upgrading to new version of R ----
+
+mypks <- readRDS("~/mypks.rds")
+install.packages(mypks)
